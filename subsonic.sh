b1d4fe0533dc3d105d3e435c00f26f9310f11ce5
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-05-04 01:16:24 +0300

    Update subsonic.sh

--- subsonic.sh
+++ subsonic.sh
@@ -1,6 +1,6 @@
-sudo apt install openjdk-8-jre --yes
+sudo apt install openjdk-9-jre --yes
 cd /tmp
-wget https://s3-eu-west-1.amazonaws.com/subsonic-public/download/subsonic-6.1.3.deb
+wget https://s3-eu-west-1.amazonaws.com/subsonic-public/download/subsonic-6.1.1.deb
 dpkg -i subsonic-6.1.3.deb
 service subsonic start
 rm subsonic-6.1.3.deb
