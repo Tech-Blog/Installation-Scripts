24cf8829849b845cb932b8745464262fba8b91ba
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-30 02:34:52 +0300

    Update and rename install_cloud9.sh to install_c9.sh

--- install_c9.sh
+++ install_c9.sh
@@ -6,13 +6,4 @@ git clone https://github.com/c9/core.git c9sdk
 cd c9sdk/
 scripts/install-sdk.sh
 wget https://raw.githubusercontent.com/Tech-Blog/Installation-Scripts/master/c9.service -O 	/etc/systemd/system/c9.service
-echo what is your username?
-read varname
-echo what is your password?
-read varpass
-sed -i -e 's/[user]/$varname/g' /etc/systemd/system/c9.service
-sed -i -e 's/[password]/$varpass/g' /etc/systemd/system/c9.service
 chmod -R 1777 /home/homeassistant/
-sudo systemctl daemon-reload
-sudo systemctl enable c9
-sudo systemctl start c9
